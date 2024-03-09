c8b25a0c682f15e6fceb8b0ec155664c255424a6
Mani Naik <maninaik111@gmail.com>
2024-03-10 01:17:23 +0530

    added mobile nav for mobile screens

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,5 +1,6 @@
+'use client'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 
 const navList = [
 	{ name: 'About', url: '#about' },
@@ -9,10 +10,14 @@ const navList = [
 ]
 
 const Nav = () => {
+	const [isSideMenuOpen, setIsSideMenuOpen] = useState(false)
+
+	const toggleMenu = () => setIsSideMenuOpen(prevVal => !prevVal)
+
 	return (
-		<header className="h-16 flex items-center">
-			<div className="flex ml-auto">
-				<ol className="flex flex-row ml-auto font-mono pr-16">
+		<header className="h-16 flex items-center justify-end md:justify-normal ">
+			<div className="ml-auto hidden md:flex pr-4 md:pr-0">
+				<ul className="flex flex-row ml-auto font-mono pr-16">
 					{navList.map(({ name, url }, index) => (
 						<li
 							key={url}
@@ -25,8 +30,45 @@ const Nav = () => {
 							</Link>
 						</li>
 					))}
-				</ol>
+				</ul>
 			</div>
+
+			<button
+				onClick={toggleMenu}
+				className="pr-4 z-20">
+				<div className="hamburger-menu">
+					<div
+						className={`hamburger-menu_line ${
+							isSideMenuOpen ? 'close-icon' : ''
+						}`}></div>
+				</div>
+			</button>
+
+			<aside
+				className="side-menu z-10 scroll-smooth"
+				style={{
+					transform: isSideMenuOpen
+						? 'translateX(0%)'
+						: 'translateX(100%)',
+				}}>
+				<ul className="flex flex-col font-mono gap-5">
+					{navList.map(({ name, url }, index) => (
+						<li
+							key={url}
+							className="p-2 text-sm hover:text-mn-green duration-100">
+							<Link
+								href={url}
+								onClick={() => setIsSideMenuOpen(false)}
+								className="flex flex-col gap-3 items-center text-white text-lg">
+								<div className="text-mn-green">{`0${
+									index + 1
+								}.`}</div>{' '}
+								{name}
+							</Link>
+						</li>
+					))}
+				</ul>
+			</aside>
 		</header>
 	)
 }
